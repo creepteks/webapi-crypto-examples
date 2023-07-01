a2cc8fde180e2becd26efd88a0c19101c0a7ec0b
Neo <mahdad.baghani@outlook.com>
2023-07-01 09:56:15 +0330

    Update index.js
    fixing some comments

--- index.js
+++ index.js
@@ -12,36 +12,36 @@ const {
     generateCounter
 } = require("./crypto")
 
-async function testKrypto() {
-    // WEB-APP SIDE
-    // create voting key
+async function ecdhScenario() {
+    // VOTING INITIALIZER SIDE
+    // create voting protocol key
     let key = await genEcdhKey()
 
-    // exporting voting key to be sent to somewhere public
+    // exporting voting public key to be sent to somewhere public
     let serverExportKey = await exportJsonWebKey(key.publicKey)
     let serverPubkeyStorage = convertJwkToJson(serverExportKey)
+    // exporting voting private key to be stored somewhere safe
     serverExportKey = await exportJsonWebKey(key.privateKey)
     let serverPrivkeyStorage = convertJwkToJson(serverExportKey)
 
-    // CLIENT SIDE: has no idea of the server priv key, has only server pubkey as JWK
-    let review = "resist"
+    // CLIENT (VOTER) SIDE: has only server pubkey as Json
+    let vote = "resist"
     let jwkObj = convertJsonToJwk(serverPubkeyStorage)
     let votingPubkey = await importEcdhJsonWebKey(jwkObj)
+    console.log(`assert imported voting pub key equals the initial one: ${votingPubkey === key.publicKey}`)
 
     let ephemeralKeyPair = await genEcdhKey()
     let clientSharedKey = await deriveSecretKey(ephemeralKeyPair.privateKey, votingPubkey)
 
-    // let iv = generateAESIV()
-    // let ivStorage = iv.toString();
     let counter = generateCounter()
     let counterStorage = counter.toString()
-    let encodedMsg = getMessageEncoding(review)
+    let encodedMsg = getMessageEncoding(vote)
     let cipherArrBuffer = await encrypt(clientSharedKey, encodedMsg, counter)
     let ciphertext = new Uint8Array(cipherArrBuffer, 0, cipherArrBuffer.byteLength).toString()
     let clientExportKey = await exportJsonWebKey(ephemeralKeyPair.publicKey)
     let clientStorage = convertJwkToJson(clientExportKey)
-    console.log(new TextEncoder().encode(ciphertext).length)
-    // WEB-APP SIDE: has the encrypted msg, JWK of the sender and the IV to the AES-GCM encryption
+    
+    // at this point, we assume the priv key of the voting is published. 
     jwkObj = convertJsonToJwk(clientStorage)
     let clientPubKey = await importEcdhJsonWebKey(jwkObj)
     let serverPrivKey = await importEcdhJsonWebKey(convertJsonToJwk(serverPrivkeyStorage), ["deriveKey"])
